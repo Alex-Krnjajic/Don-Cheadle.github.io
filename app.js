c628c671d311e3b4115c38fe587720787bca62e7
Alex <alexk753@gmail.com>
2020-08-24 14:08:26 +0200

    id

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 
+const peerOnOpen = (id) => {
+  document.querySelector('.my-peer-id').innerHTML(id);
+}
+
+const myPeerId = location.hash;
+console.log(myPeerId);
+let peer = new Peer(null, {
+  host: "glajan.com",
+  port: 8443,
+  path:"/myapp",
+  secure: true,
+  });
+
+peer.on("open", peerOnOpen)
