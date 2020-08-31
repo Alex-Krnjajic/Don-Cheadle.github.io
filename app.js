a97b80149468a9446d1435c2d89faaf37c6184a6
Alex <alexk753@gmail.com>
2020-08-31 10:22:05 +0200

    Connection

--- app.js
+++ app.js
@@ -1,49 +1,61 @@
-const peerOnOpen = (id) => {
-  document.querySelector('.my-peer-id').innerHTML = id;
-};
+(function () {
+  const peerOnOpen = (id) => {
+    document.querySelector('.my-peer-id').innerHTML = id;
+  };
 
-const peerOnError = (err) => {
-  console.log(err);
-}
-
-const myPeerId = location.hash.slice(1);
-console.log(myPeerId);
-
-let peer = new Peer(myPeerId, {
-  host: "glajan.com",
-  port: 8443,
-  path: "/myapp",
-  secure: true,
-});
-
-let yoshi = (size) => {
-  output = "b";
-  for (var i = 0; i < size; i++) {
-    output += "e";
+  const peerOnError = (err) => {
+    console.log(err);
   }
-  output += "g yoshi";
-  document.querySelector('.yoshi').innerHTML = output
-};
+
+  const myPeerId = location.hash.slice(1);
+  console.log(myPeerId);
+
+  const connectToPeerClick = (el) => {
+    const peerId = el.target.textContent;
+    const conn = peer.connect(peerId);
+    conn.on('open', () => {
+    console.log("connected");
+    });
+  }
+
+  let peer = new Peer(myPeerId, {
+    host: "glajan.com",
+    port: 8443,
+    path: "/myapp",
+    secure: true,
+  });
+
+  let yoshi = (size) => {
+    output = "b";
+    for (var i = 0; i < size; i++) {
+      output += "e";
+    }
+    output += "g yoshi";
+    document.querySelector('.yoshi').innerHTML = output
+  };
 
 
-peer.on("open", peerOnOpen);
-peer.on('error', peerOnError)
+  peer.on("open", peerOnOpen);
+  peer.on('error', peerOnError)
 
-document.querySelector('.list-all-peers-button').addEventListener('click', () => {
-  const peersEl = document.querySelector('.peers');
-  peer.listAllPeers((peers) => {
-    const ul = document.createElement('ul');
-    peers.filer((peerId) => peerId !== myPeerId)
-      .forEach((peerId) => {
-        const li = document.createElement('li');
-        const button = document.createElement('button');
-        button.innerText = peerId;
-        button.classList.add("connect-button");
-        button.classList.add(`peerId-${peerId}`);
-        li.appendChild(button);
-        ul.appendChild(li);
-      });
-    peersEl.appendChild(ul);
+  document.querySelector('.list-all-peers-button').addEventListener('click', () => {
+    const peersEl = document.querySelector('.peers');
+    peer.listAllPeers((peers) => {
+      const ul = document.createElement('ul');
+      peers
+        .filter((peerId) => peerId !== myPeerId)
+        .forEach((peerId) => {
+          const li = document.createElement('li');
+          const button = document.createElement('button');
+          button.innerText = peerId;
+          button.classList.add("connect-button");
+          button.classList.add(`peerId-${peerId}`);
+          button.addEventListener('click', connectToPeerClick);
+          li.appendChild(button);
+          ul.appendChild(li);
+        });
+      peersEl.appendChild(ul);
 
+    })
   })
-})
+})();
