c96de047b10bbe45f951d4cda9b94375c405a6a9
Alex <alexk753@gmail.com>
2020-09-01 10:19:13 +0200

    turn and ice

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 (function () {
+  let peer = null;
+  let conn = null;
   const peerOnOpen = (id) => {
     document.querySelector('.my-peer-id').innerHTML = id;
   };
@@ -10,19 +12,46 @@
   const myPeerId = location.hash.slice(1);
   console.log(myPeerId);
 
+  const peerOnConnection = (dataConnection) => {
+    conn && conn.close();
+    conn = dataConnection;
+    console.log(dataConnection); 
+    
+    const event = new CustomEvent("peer-changed",{
+      detail: { peerId: dataConnection },
+    });
+    document.dispatchEvent(event);
+  };
+
   const connectToPeerClick = (el) => {
     const peerId = el.target.textContent;
-    const conn = peer.connect(peerId);
+    conn && conn.close();
+    conn = peer.connect(peerId);
     conn.on('open', () => {
-    console.log("connected");
+      console.log("connected");
+      const event = new CustomEvent("peer-changed",{
+          detail: { peerId: peerId },
+        });
+        document.dispatchEvent(event);
     });
-  }
+  };
 
-  let peer = new Peer(myPeerId, {
+  peer = new Peer(myPeerId, {
     host: "glajan.com",
     port: 8443,
     path: "/myapp",
     secure: true,
+    config: {
+      iceServers: [
+        { url: ["stun:eu-turn7.xirsys.com"] },
+        {
+          username:
+            "1FOoA8xKVaXLjpEXov-qcWt37kFZol89r0FA_7Uu_bX89psvi8IjK3tmEPAHf8EeAAAAAF9NXWZnbGFqYW4=",
+          credential: "83d7389e-ebc8-11ea-a8ee-0242ac140004",
+          url: "turn:eu-turn7.xirsys.com:80?transport=udp",
+        },
+      ],
+    },
   });
 
   let yoshi = (size) => {
@@ -36,10 +65,13 @@
 
 
   peer.on("open", peerOnOpen);
-  peer.on('error', peerOnError)
+  peer.on("error", peerOnError);
+  peer.on("connection", peerOnConnection);
+
 
   document.querySelector('.list-all-peers-button').addEventListener('click', () => {
     const peersEl = document.querySelector('.peers');
+    peersEl.firstChild && peersEl.firstChild.remove();
     peer.listAllPeers((peers) => {
       const ul = document.createElement('ul');
       peers
@@ -57,5 +89,14 @@
       peersEl.appendChild(ul);
 
     })
+  })
+  document.addEventListener('peer-changed', (e) => {
+    const peerId = e.detail.peerId;
+    console.log(peerId);
+    let peerIdClass = '.peerId-'+peerId;
+    document.querySelectorAll(".connect-button").forEach((el) => {
+      el.classList.remove("connected");
+    });
+    document.querySelector(peerIdClass).classList.add("connected");
   })
 })();
